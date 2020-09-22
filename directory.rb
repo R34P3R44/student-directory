5dff52a2d03ebf78bb1898cef9cf49b1bd0cb266
Adrian Toth <t.adrian88@gmail.com>
2020-09-22 14:15:06 +0100

    User input added

--- directory.rb
+++ directory.rb
@@ -1,5 +1,24 @@
+def input_students
+  puts "Please neter the names of the students"
+  puts "To finish, just hit return twice"
+  # Adding an empty array
+  students = []
+  # Getting first user input
+  name = gets.chomp
+  # Adding the while loop
+  while !name.empty? do
+    # Adding the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # Getting second user input
+    name = gets.chomp
+  end
+  # Returning student array
+  students
+end
 # Adding all students into an array
-students = [
+=begin
+ students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
   {name: "Nurse Ratched", cohort: :november},
@@ -12,20 +31,25 @@ students = [
   {name: "Joeffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+=end
+# We start printing the information
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The Villains of my cohort at Makers"
   puts "-------------"
 end
+
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 # We call in some methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
